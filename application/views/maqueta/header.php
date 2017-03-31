f9dbc498d83dddc1a45714fc15aae6d95cd2e507
darroylat <darroylat@gmail.com>
2017-03-30 21:21:04 -0300

    Linea Base

---
+++ application/views/maqueta/header.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
---
+++ application/views/template_principal.php
@@ -0,0 +1,4 @@
+<?php $this->load->view($encabezadoHtml); ?>
+<?php $this->load->view($cabeceraHtml,$title); ?>
+<?=$contenido?>
+<?php $this->load->view($footerHtml); ?>
