b623ab733ab0af9b6c9171b2ce0551993669fcdf
darroylat <darroylat@gmail.com>
2017-05-02 20:13:09 -0300

    pequeños cambios

--- application/views/principal/banner.php
+++ application/views/principal/banner.php
@@ -1,7 +1,8 @@
 <!-- Banner -->
 				<section id="banner">
-					<h2>Mountain</h2>
-					<p>Another fine responsive site template freebie by HTML5 UP.</p>
+					<img src="<?php echo base_url(); ?>images/mountain.png" width="200"  alt="">
+					<!--h2>Mountain</h2-->
+					<!--p>Another fine responsive site template freebie by HTML5 UP.</p-->
 					<ul class="actions">
 						<li><a href="<?php echo base_url(); ?>principal/ingresa" class="button special">Ingresar</a></li>
 						<li><a href="<?php echo base_url(); ?>principal/contacto" class="button">Contáctenos</a></li>
