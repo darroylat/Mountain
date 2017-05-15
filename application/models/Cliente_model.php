1f2e11ee302aecc6bc8e389a8a45b35c25f456e5
darroylat <darroylat@gmail.com>
2017-05-15 08:07:30 -0400

    CAMBIOS

--- application/models/Cliente_model.php
+++ application/models/Cliente_model.php
@@ -13,16 +13,13 @@ class Cliente_model extends CI_Model {
         }
 
         public function select_cuenta_usuario($usuario, $contrasena){
-                $this->usuariocliente = $this->input->post('user');
-                $this->passcliente    = $this->input->post('pass');
-
               $query = $this->db->get_where('cliente', array('usuariocliente' => $usuario, 'passcliente' => $contrasena));
               return $query->row_array();
         }
 
         //Funcionando
         public function get_where_one($id){
-            $query = $this->db->get_where('cliente',array('idcliente' => $id));
+            $query = $this->db->get_where('cliente', array('idcliente' => $id));
             return $query->row_array();
         }
 
